d4c553d7f81a1c90700a64e42c3a8c049685d3ba
haivvd <vovandaihai@gmail.com>
2022-02-24 15:12:20 +0700

    this folder for crawl website with scrapy

---
+++ product_scraper/product_scraper/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProductScraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    id = scrapy.Field()
+    price = scrapy.Field()
+    salePrice = scrapy.Field()
+    images = scrapy.Field()
+    size = scrapy.Field()
+    color = scrapy.Field()
+    link = scrapy.Field()
+    description = scrapy.Field()
